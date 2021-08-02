bab8fe1c44916ee09e77ffa005ee0ee7b2d6ac55
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-02 17:58:42 +0200

    Add hitRecord factory function

--- src/lib/shipFactory.js
+++ src/lib/shipFactory.js
@@ -1,5 +1,14 @@
+const buildHitRecord = (shipLength) => {
+  let hitRecord = {};
+  for ( let i = 0; i < shipLength; i++) {
+    hitRecord[i] = "clear";
+  }
+  return hitRecord;
+};
+
 const shipFactory = (length) => {
-  return { length };
+  const hitRecord = buildHitRecord(length); 
+  return { length, hitRecord , sunk: false };
 };
 
 export default shipFactory;
