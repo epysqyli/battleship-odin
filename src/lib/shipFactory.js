38ad779697c7379c5afad5073924bf5bdd3af20d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-04 17:54:57 +0200

    Apply arrow syntax to shipFactory

--- src/lib/shipFactory.js
+++ src/lib/shipFactory.js
@@ -1,3 +1,4 @@
+
 const buildHitRecord = (shipLength) => {
   let hitRecord = {};
   for (let i = 0; i < shipLength; i++) {
@@ -6,20 +7,20 @@ const buildHitRecord = (shipLength) => {
   return hitRecord;
 };
 
-function hit(pos) {
-  this.hitRecord[pos] = "hit";
-}
-
-function isSunk() {
-  if (Object.values(this.hitRecord).every((pos) => pos === "hit")) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 const shipFactory = (name, length, quantity) => {
   const hitRecord = buildHitRecord(length);
+
+  const hit = (pos) => {
+    hitRecord[pos] = "hit";
+  };
+
+  const isSunk = () => {
+    if (Object.values(hitRecord).every((pos) => pos === "hit")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   return {
     name,
     length,
