d9d4b9060baa6d386ceb1fb5712bd0519d8b87ce
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-05 15:54:31 +0200

    Add computer player randomAttack function

--- src/lib/player.js
+++ src/lib/player.js
@@ -2,10 +2,25 @@ import gameboard from "./gameboard";
 
 const createPlayer = (playerName) => {
   const playerBoard = gameboard(playerName);
+  
   const attack = (x, y, enemyBoard) => {
     enemyBoard.receiveAttack(x, y);
   };
-  return { playerBoard, attack };
+
+  let randomMoves = [];
+  const randomAttack = (enemyBoard) => {
+    let x = Math.floor(Math.random() * 10) + 1;
+    let y = Math.floor(Math.random() * 10) + 1;
+
+    while (randomMoves.includes([x, y])) {
+      let x = Math.floor(Math.random() * 10) + 1;
+      let y = Math.floor(Math.random() * 10) + 1;
+    }
+
+    randomMoves.push([x, y]);
+    enemyBoard.receiveAttack(x, y);
+  };
+  return { playerBoard, attack, randomAttack };
 };
 
 export default createPlayer;
