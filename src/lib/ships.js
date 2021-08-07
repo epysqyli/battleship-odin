9ff47152f019e02130fb4280cf60a549cd5297b1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-07 17:45:39 +0200

    Remove unused argument in lib/ships

--- src/lib/ships.js
+++ src/lib/ships.js
@@ -1,11 +1,11 @@
 import { shipFactory } from "./shipFactory";
 
 const ships = [
-  { carrier: shipFactory("carrier", 5, 1) },
-  { battleship: shipFactory("battleship", 4, 1) },
-  { cruiser: shipFactory("cruiser", 3, 1) },
-  { submarine: shipFactory("submarine", 3, 1) },
-  { destroyer: shipFactory("destroyer", 2, 1) },
+  { carrier: shipFactory("carrier", 5) },
+  { battleship: shipFactory("battleship", 4) },
+  { cruiser: shipFactory("cruiser", 3) },
+  { submarine: shipFactory("submarine", 3) },
+  { destroyer: shipFactory("destroyer", 2) },
 ];
 
 export default ships;
